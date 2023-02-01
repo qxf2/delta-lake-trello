6040ba4689208a9c9d44c66a35c1c9896850d308
sravantit25 <62924721+sravantit25@users.noreply.github.com>
2023-02-01 11:38:24 +0530

    Update delta_table_conf.py

--- config/delta_table_conf.py
+++ config/delta_table_conf.py
@@ -2,8 +2,8 @@
 Contains the paths of the delta tables
 """
 
-cards_bronze_table_path = '/code/lakehouse_project/data_tables/bronze_cards_table'
-members_bronze_table_path = '/code/lakehouse_project/data_tables/bronze_members_table'
-cards_silver_table_path = '/code/lakehouse_project/data_tables/silver_cards_table'
-noactive_cards_gold_path = '/code/lakehouse_project/data_tables/gold_noactive_cards_table'
+cards_bronze_table_path = '~/code/lakehouse_project/data_tables/bronze_cards_table'
+members_bronze_table_path = '~/code/lakehouse_project/data_tables/bronze_members_table'
+cards_silver_table_path = '~/code/lakehouse_project/data_tables/silver_cards_table'
+noactive_cards_gold_path = '~/code/lakehouse_project/data_tables/gold_noactive_cards_table'
 

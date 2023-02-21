10d7199a46b694b6faf28c44f16414b3956f0a03
akkuldn <akkul.dn@qxf2.com>
2023-02-21 11:34:08 +0530

    Updated username

--- jobs/akkul_cards_gold_path.py
+++ jobs/akkul_cards_gold_path.py
@@ -39,7 +39,7 @@ def get_user_cards(spark, start_date, end_date):
         refined_cards_data = refined_cards_data.withColumn("dateLastActivity", to_date(from_utc_timestamp("dateLastActivity", "UTC")))
 
         # Filter the DataFrame for the given user and date range
-        filtered_cards_data = refined_cards_data.filter((col("card_members").like("%Akkul%")) & (col("dateLastActivity") >= start_date) & (col("dateLastActivity") <= end_date))
+        filtered_cards_data = refined_cards_data.filter((col("card_members").like("%akkuldn%")) & (col("dateLastActivity") >= start_date) & (col("dateLastActivity") <= end_date))
         logger.info(f'Fetched user card data within the date range')  
 
         #Select the required columns
